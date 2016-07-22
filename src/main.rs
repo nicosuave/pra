03ad5c3f55c3713c9e6186621579db187328e8e4
nicosuave <ritschel@gmail.com>
2016-07-22 09:14:53 -0700

    Add config parsing WIP

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+#![feature(plugin)]
+#![plugin(serde_macros)]
+#![feature(custom_derive)]
+#![feature(custom_attribute)]
+
 extern crate serde;
 extern crate serde_json;
-extern crate regex;
 
+use std::env;
 use std::fs::File;
 use std::io::Read;
-use serde_json::Value;
-use serde_json::Error;
-use std::env;
 
 fn main() {
     read_config();
@@ -19,9 +21,10 @@ fn read_config() {
                 Ok(mut v) => {
                     let mut config_string = String::new();
                     &v.read_to_string(&mut config_string).unwrap();
-                    let deserialized: Result<Value, Error> = serde_json::from_str(&config_string);
+                    let deserialized: Result<Foo, serde_json::Error> = serde_json::from_str(&config_string);
+                    // deserialized.unwrap();
                     match deserialized {
-                        Ok(v) => println!("Your ~/.pra.config was read successfully: {}", v),
+                        Ok(v) => println!("Your ~/.pra.config was read successfully: {:?}", v.foo),
                         Err(_) => println!("Your ~/.pra.config is invalid")
                     }
                 },
@@ -30,4 +33,58 @@ fn read_config() {
         }
         None => println!("Could not find your home directory!")
     }
+}
+
+// {
+//   "pull_sources": [
+//     {
+//       "type": "github",
+//       "config": {
+//         "protocol": "https",
+//         "host": "api.github.com",
+//         "username": "your.username",
+//         "password": "your.password",
+//         "repositories": [
+//           { "owner": "reachlocal", "repository": "snapdragon" },
+//           { "owner": "brewster", "repository": "cequel" }
+//         ]
+//       }
+//     }
+//   ]
+// }
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+struct Foo {
+    foo: String
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+struct PraConfig {
+    pull_sources: Vec<PullSource>
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+struct PullSource {
+    service: String,
+    pull_sources: Vec<Config>
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+struct Config {
+    protocol: String,
+    host: String,
+    username: String,
+    password: String,
+    repositories: Vec<Repository>
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+struct Repository {
+    owner: String,
+    repository: String
 }
