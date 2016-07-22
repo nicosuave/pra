fddcd269bdb4d528542f784b1336e8bd784b90a8
nicosuave <ritschel@gmail.com>
2016-07-22 01:11:50 -0700

    Add reading of ~/.pra.json

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-extern crate argparse;
+extern crate serde;
+extern crate serde_json;
+extern crate regex;
 
-use argparse::{ArgumentParser, StoreTrue};
+use std::fs::File;
+use std::io::Read;
+use serde_json::Value;
+use serde_json::Error;
+use std::env;
 
 fn main() {
-    let mut should_authenticate = false;
-
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("PRA – the pull request aggregator");
-        ap.refer(&mut should_authenticate).add_option(&["--a", "--authenticate"], StoreTrue, "Authenticate");
-        ap.parse_args_or_exit();
-    }
-
-    if should_authenticate {
-        authenticate()
-    }
+    read_config();
 }
 
-fn authenticate() {
-    println!("Looks like you want to authenticate!");
+fn read_config() {
+    match env::home_dir() {
+        Some(path) => {
+            match File::open(format!("{}/.pra.json", path.display())) {
+                Ok(mut v) => {
+                    let mut config_string = String::new();
+                    &v.read_to_string(&mut config_string).unwrap();
+                    let deserialized: Result<Value, Error> = serde_json::from_str(&config_string);
+                    match deserialized {
+                        Ok(v) => println!("Your ~/.pra.config was read successfully: {}", v),
+                        Err(_) => println!("Your ~/.pra.config is invalid")
+                    }
+                },
+                Err(_) => println!("Could not read your ~/pra.json")
+            }
+        }
+        None => println!("Could not find your home directory!")
+    }
 }
